ca2c1958c467f65230733c8e7d03d26868982c38
Brett <brett@bretthales.com>
2019-04-09 14:08:00 +1000

    Uploaded new one

--- Apex/WelcomeViewController.swift
+++ Apex/WelcomeViewController.swift
@@ -13,104 +13,86 @@ import KeyboardMan
 import IBAnimatable
 
 class welcomeViewController: UIViewController {
-    
-    
-    @IBOutlet weak var scroll: UIScrollView!
+ 
+    // IB Outlets
     @IBOutlet weak var logoImage: UIImageView!
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var countryPicker: UIView!
+    @IBOutlet weak var countryCodeLabel: UILabel!
+    @IBOutlet weak var countryCodeLine: UIView!
     @IBOutlet weak var phoneNumberTextField: HoshiTextField!
-    @IBOutlet weak var countryButton: UIButton!
-    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var continueButton: AnimatableButton!
+    @IBOutlet weak var continueButtonHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var phoneNumberLabelError: UILabel!
+    @IBOutlet weak var countryCodeButton: UIButton!
+    
+    
+    
     
     let keyboardMan = KeyboardMan()
     
-    var validate: Bool = false {
-        didSet {
-            if validate {
-                self.continueButton.fillColor = UIColor.green
-            }
-            else {
-                self.continueButton.fillColor = UIColor.lightGray
-            }
-        }
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         self.Initialize()
         self.InitializeKeyboardMan()
-        self.dismiss(animated: true, completion: nil)
-        
-      
     }
     
     
-    //MARK: Initialize UI properties
-    func Initialize(){
-        
+    @IBAction func continueButton(_ sender: Any) {
+      //  if self.validate {
+      //      self.performSegue(withIdentifier: "", sender: self)
+       // }
+    }
+    
+    // MARK: used for viewing end and editing when touches view
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    func Initialize() {
         self.continueButton.fillColor = UIColor.lightGray
         
-        phoneNumberTextField.delegate = self
+      //  phoneNumberTextField.delegate = self
+        
+        self.phoneNumberLabelError.isHidden = true
         
     }
     
-    // MARK: Keyboard Man Initializer
     func InitializeKeyboardMan() {
+        self.continueButtonHeightConstraint.constant = 50
         
-        self.bottomConstraint.constant = 50
-        
-        // - When keyboard will show
+        // When the keyboard will show
         self.keyboardMan.animateWhenKeyboardAppear = { [weak self] appearPostIndex, keyboardHeight, keyboardHeightIncrement in
             
-            print("Appear height: \(keyboardHeight) ===== \(keyboardHeightIncrement)")
+            print("Appear height: \(keyboardHeight) ====== \(keyboardHeightIncrement)")
             
             if let self = self {
-            
-            self.widthConstraint.constant = self.view.frame.width
-            self.bottomConstraint.constant = keyboardHeight
-            UIView.animate(withDuration: 0.5) {
-                self.view.layoutIfNeeded()
-            }
-        }
-        
-    }
-        // When keyboard will hide
-        self.keyboardMan.animateWhenKeyboardDisappear = { [weak self] keyboardHeight in
-            print("dismiss height: \(keyboardHeight)")
-            
-            if let self = self {
-                self.widthConstraint.constant = 300
-                self.bottomConstraint.constant = 50
+                
+                self.widthConstraint.constant = self.view.frame.width
+                self.continueButtonHeightConstraint.constant = keyboardHeight
                 UIView.animate(withDuration: 0.5) {
                     self.view.layoutIfNeeded()
                 }
             }
-
         }
-    }
-    
-    //MARK: Checking Phone Number Valid
-    func isValidPhoneNumber(value: String) -> Bool {
-        let PHONE_REGEX = "^((\\+)|(00))[0-8]{6,14}$"
-        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
-        let result =  phoneTest.evaluate(with: value)
-        return result
-    }
-    
-}
-
-extension welcomeViewController: UITextFieldDelegate {
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        return true
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        return true
+        
+        // When keyboard will hide
+        self.keyboardMan.animateWhenKeyboardDisappear = { [weak self] keyboardHeight in
+            
+            print("dismiss height: \(keyboardHeight)")
+            
+            if let self = self {
+                
+                self.widthConstraint.constant = 300
+                self.continueButtonHeightConstraint.constant = 50
+                UIView.animate(withDuration: 0.5) {
+                    self.view.layoutIfNeeded()
+                }
+            }
+        }
     }
     
 }
