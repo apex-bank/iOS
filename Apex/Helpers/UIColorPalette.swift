45ed8e1ebb9ab92deff721ac8c4bcfb348023eff
Brett <brett@bretthales.com>
2019-04-10 11:36:24 +1000

    Added some more things with collection view cells
    Apex at work

---
+++ Apex/Helpers/UIColorPalette.swift
@@ -0,0 +1,36 @@
+//
+//  UIColorPalette.swift
+//  Apex
+//
+//  Created by Brett Hales on 4/10/19.
+//  Copyright © 2019 Brett Hales. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var DarkGreyBlue: UIColor {
+        return UIColor(red: 46.0 / 255.0, green: 59.0 / 255.0, blue: 78.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var darkNavyBlue22: UIColor {
+        return UIColor(red: 0.0, green: 0.0, blue: 25.0 / 255.0, alpha: 0.22)
+    }
+    
+    @nonobjc class var deepSkyBlue: UIColor {
+        return UIColor(red: 0.0, green: 122.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var deepSkyBlue25: UIColor {
+        return UIColor(red: 0.0, green: 122.0 / 255.0, blue: 1.0, alpha: 0.25)
+    }
+    
+    @nonobjc class var darkSkyBlue: UIColor {
+        return UIColor(red: 74.0 / 255.0, green: 144.0 / 255.0, blue: 226.0 / 255.0, alpha: 1.0)
+    }
+    
+    
+    
+}
--- Apex/Passcode/PasscodeViewController.swift
+++ Apex/Passcode/PasscodeViewController.swift
@@ -29,10 +29,12 @@ class passcodeViewController: UIViewController {
         passCodeKeyStruct(status: 0, title: "7", value: "7"),
         passCodeKeyStruct(status: 0, title: "8", value: "8"),
         passCodeKeyStruct(status: 0, title: "9", value: "9"),
-        passCodeKeyStruct(status: 1, title: "Forgot", value: ""),
+        passCodeKeyStruct(status: 1, title: "Forgot", value: "Forgot"),
         passCodeKeyStruct(status: 0, title: "0", value: "0"),
-        passCodeKeyStruct(status: 1, title: "Delete", value: ""),
+        passCodeKeyStruct(status: 2, title: "Delete", value: "Delete"),
         ]
+    
+    var inputPassCode : [String] = []
 
     
     let PasscodeNumberPadCollectionViewCell = "PasscodeNumberPadCollectionViewCell"
@@ -43,28 +45,80 @@ class passcodeViewController: UIViewController {
     @IBOutlet weak var passcodeLabel: UILabel!
     @IBOutlet weak var passcodeCollectionView: UICollectionView!
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var passcode1: UIView!
+    @IBOutlet weak var passcode2: UIView!
+    @IBOutlet weak var passcode3: UIView!
+    @IBOutlet weak var passcode4: UIView!
+    
+    private func setupKeycodeView(){
+        
+        self.passcode1.layer.cornerRadius = 10
+        self.passcode1.backgroundColor = UIColor.deepSkyBlue25
+        self.passcode2.layer.cornerRadius = 10
+        self.passcode2.backgroundColor = UIColor.deepSkyBlue25
+        self.passcode3.layer.cornerRadius = 10
+        self.passcode3.backgroundColor = UIColor.deepSkyBlue25
+        self.passcode4.layer.cornerRadius = 10
+        self.passcode4.backgroundColor = UIColor.deepSkyBlue25
+    }
     
     private func setupCollectionView(){
     self.passcodeCollectionView.register(UINib(nibName:self.PasscodeNumberPadCollectionViewCell, bundle: nil), forCellWithReuseIdentifier: self.PasscodeNumberPadCollectionViewCell)
     self.passcodeCollectionView.register(UINib(nibName:self.PasscodeNumberPadTextCollectionViewCell, bundle: nil), forCellWithReuseIdentifier: self.PasscodeNumberPadTextCollectionViewCell)
     
     
-     //  self.passcodeCollectionView.datasource = self
+     //   self.passcodeCollectionView.datasource = self
         self.passcodeCollectionView.delegate = self
     
+    }
+    
+    func clearInputKeycodeView(){
+    
+    self.inputPassCode = []
+    self.inputPassCode = []
+    self.setupKeycodeView()
+    
+    }
+    
+    func inputPassCodeAction() {
+        
+       self.setupKeycodeView()
+        
+        if(self.inputPassCode.count == 1){
+            self.passcode1.layer.cornerRadius = 10
+            self.passcode1.backgroundColor = UIColor.deepSkyBlue
+        }
+        if(self.inputPassCode.count == 2){
+            self.passcode1.backgroundColor = UIColor.deepSkyBlue
+            self.passcode2.backgroundColor = UIColor.deepSkyBlue
+            
+        }
+        if(self.inputPassCode.count == 3){
+            self.passcode1.backgroundColor = UIColor.deepSkyBlue
+            self.passcode2.backgroundColor = UIColor.deepSkyBlue
+            self.passcode3.backgroundColor = UIColor.deepSkyBlue
+            
+        }
+        if(self.inputPassCode.count == 4){
+            self.passcode1.backgroundColor = UIColor.deepSkyBlue
+            self.passcode2.backgroundColor = UIColor.deepSkyBlue
+            self.passcode3.backgroundColor = UIColor.deepSkyBlue
+            self.passcode4.backgroundColor = UIColor.deepSkyBlue
+            
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        self.clearInputKeycodeView()
         self.setupCollectionView()
+        
+     
     }
-    
-    
-    
 }
+
 
 extension passcodeViewController : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
@@ -76,7 +130,7 @@ extension passcodeViewController : UICollectionViewDataSource, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        
+     
         let data = self.passCodeKeyData[indexPath.row]
         
         if(data.status == 0) {
@@ -88,18 +142,19 @@ extension passcodeViewController : UICollectionViewDataSource, UICollectionViewD
             
             return numCell
             
-            
         }
         
-        
+       
         // For Numberpad Text (i.e. Forgot PIN?)
         let textCell = collectionView.dequeueReusableCell(withReuseIdentifier: PasscodeNumberPadTextCollectionViewCell, for: indexPath) as! PasscodeNumberPadTextCollectionViewCell
         
         textCell.setupCell(title: data.title)
         
         return textCell
+        }
+    
         
-    }
+    
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -110,4 +165,29 @@ extension passcodeViewController : UICollectionViewDataSource, UICollectionViewD
         return CGSize(width: width, height: height)
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let data = self.passCodeKeyData[indexPath.row]
+        
+        if(data.status == 0){
+            
+            self.inputPassCode.append(data.value)
+            self.inputPassCodeAction()
+            
+        }
+        else {
+            if(data.status == 1) {
+                self.clearInputKeycodeView()
+            }
+            
+            if(data.status == 2) {
+                if(self.inputPassCode.count > 0){
+                    
+                    self.inputPassCode.remove(at: inputPassCode.count - 1)
+                    
+                    self.inputPassCodeAction()
+                }
+            }
+        }
+    }
 }
